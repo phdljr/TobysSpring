a76c56b02ad8da0df319c9b1fdc53989ea88a123
phdljr <phdljr@naver.com>
2022-10-13 06:33:11 +0900

    push

--- src/main/java/tobyspring/user/dao/UserDaoTest.java
+++ src/main/java/tobyspring/user/dao/UserDaoTest.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao(new DConnectionMaker());
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
         user.setId("111");
