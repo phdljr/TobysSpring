4031ba5b71234fef449e7cc236b58a64f804a0a4
phdljr <phdljr@naver.com>
2022-10-20 01:07:08 +0900

    push

--- src/test/java/learningtest/template/jdk/ReflectionTest.java
+++ src/test/java/learningtest/template/jdk/ReflectionTest.java
@@ -1,9 +1,14 @@
 package learningtest.template.jdk;
 
 import org.junit.jupiter.api.Test;
+import tobyspring.learningtest.reflection.Hello;
+import tobyspring.learningtest.reflection.HelloTarget;
+import tobyspring.learningtest.reflection.HelloUppercase;
+import tobyspring.learningtest.reflection.UppercaseHandler;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -21,5 +26,34 @@ public class ReflectionTest {
 
         Method charAtMethod = String.class.getMethod("charAt", int.class);
         assertThat((Character) charAtMethod.invoke(name, 0)).isEqualTo('S');
+    }
+
+    @Test
+    void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank you Toby");
+    }
+
+    @Test
+    void simpleUpperProxy() {
+        Hello hello = new HelloUppercase(new HelloTarget());
+        assertThat(hello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(hello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    void dynamicProxy() {
+        Hello hello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{Hello.class},
+                new UppercaseHandler(new HelloTarget())
+        );
+
+        assertThat(hello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(hello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
     }
 }
