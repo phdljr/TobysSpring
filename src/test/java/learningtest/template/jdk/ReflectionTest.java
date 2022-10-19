efdf93093dc01760c2750aebbc2b0aa02dc6f5c2
phdljr <phdljr@naver.com>
2022-10-20 00:07:53 +0900

    push

---
+++ src/test/java/learningtest/template/jdk/ReflectionTest.java
@@ -0,0 +1,25 @@
+package learningtest.template.jdk;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ReflectionTest {
+    @Test
+    void invokeMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String name = "Spring";
+
+        assertThat(name.length()).isEqualTo(6);
+
+        Method lengthMethod = String.class.getMethod("length");
+        assertThat((Integer)lengthMethod.invoke(name)).isEqualTo(6);
+
+        assertThat(name.charAt(0)).isEqualTo('S');
+
+        Method charAtMethod = String.class.getMethod("charAt", int.class);
+        assertThat((Character) charAtMethod.invoke(name, 0)).isEqualTo('S');
+    }
+}
